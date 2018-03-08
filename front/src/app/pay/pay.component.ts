f640b290d708557154809db5ca4e408e0d31ad57
CTS <1393676091@qq.com>
2018-03-08 17:57:22 +0800

    aaa

--- front/src/app/pay/pay.component.ts
+++ front/src/app/pay/pay.component.ts
@@ -36,7 +36,7 @@ export class PayComponent implements OnInit {
             // })
             //打印小票模板
             function receipt(dataset) {
-                var cont = `阿婆私房菜  \n*********************************\n名称     金额     数量\n`;
+                var cont = `阿婆私房菜  \n*********************************\n名称           金额           数量\n`;
                 // var time = new Date();
                 var date = new Date();
                 var year = date.getFullYear();
@@ -47,9 +47,9 @@ export class PayComponent implements OnInit {
                 var second = date.getSeconds();
                 var time = year + '/' + month + '/' + day + '/ ' + hour+':' + minute + ':' + second;
                 dataset.forEach(function (item, idx) {
-                    cont += `${item.foodName}   ${item.price}   ${item.count}\n`
+                    cont += `${item.foodName}      ${item.price}        ${item.count}\n`
                 }); 
-                cont += `总数量: ${wthis.store['count1']}件\n总金额：${wthis.store['totle']}元\n买单时间：${time}\n*************************************\n`;
+                cont += `总数量: ${wthis.store['count1']}件\n总金额：${wthis.store['totle']}元\n用户:${wthis.userPhone}\n买单时间：${time}\n*************************************\n`;
                 return cont;
 
             }
